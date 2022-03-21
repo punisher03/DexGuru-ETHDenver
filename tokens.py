070b60c34ca5050ec4d550d0e0237b95f0736116
Rishvanth Katragadda <rshk70@gmail.com>
2022-03-21 22:37:36 +0530

    Delete tokens.py

--- tokens.py
+++
@@ -1,6 +0,0 @@
-import secrets
-
-
-dexguru_token = {{ secrets.dexguru_token }} #'zbSn27Njj84LOs7F89-X6XBbvBHPHDenFNcsdYFkNbA'
-TG_TOKEN = {{secrets.TG_TOKEN}} #'5162481471:AAFMF1P1r6-m_QOjb8TqmOO1nQWY9s-bvRM'
-DB_FILENAME = 'data/db.pickle'
